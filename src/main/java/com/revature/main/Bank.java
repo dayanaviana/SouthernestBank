acbe0b246789e6425dba7f2fa1db960703527aca
Dayana Viana <dayanatsv@gmail.com>
2019-07-18 18:49:29 -0400

    Deleting Ben's files

--- src/main/java/com/revature/main/Bank.java
+++ src/main/java/com/revature/main/Bank.java
@@ -1,19 +1,10 @@
 package com.revature.main;
-import java.util.Scanner;
 
 public class Bank {
 
 	public static void main(String[] args) {
-		System.out.println("1: Apply for new account"); //Not yet implemente
-		System.out.println("2: Customer Login");
-		System.out.println("3: Employee Login"); //Not yet implemented, admin should inherit from employee class
-		Scanner input = new Scanner(System.in); //Create scanner object to get user input
-		int choice = input.nextInt(); //Get user choice
-		//call menuselection
-		
-		
-		
-		
+		System.out.println("Register new account | Login");
+
 	}
 
 }
--- src/main/java/com/revature/main/Employee.java
+++
@@ -1,5 +0,0 @@
-package com.revature.main;
-
-public class Employee extends Application{ //Should this be public
-	//This should allow an employee to approve or deny an application
-}
