1eb617e0ee54c51896d9c02a2f42a4b1e43de6fe
Dayana Viana <dayanatsv@gmail.com>
2019-07-22 20:59:52 -0400

    Customer class: adding functions

--- src/com/revature/main/Customers.java
+++ src/com/revature/main/Customers.java
@@ -4,19 +4,37 @@ import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Customers {
 	private static final String filepath = "appList.txt";
 	private static Map<String, Account> db;
 
+	//Must Instaciate Customer to load file
 	public Customers() {
 		super();
 		//load file into HASH-MAP
 		db = (HashMap)ReadObjectFromFile(filepath);
 	}
+	
+	//To Test functionalities
+	public static void main(String[] args) {
+		Customers cu = new Customers();
+		System.out.println(cu.getAllCustomers().toString());
+	}
 
+	public Set<String> getAllCustomers(){
+		return db.keySet();
+	}
+	
+	//delete Customer and his account
+	public boolean deleteCustomer(String name) {
+		return db.remove(name,db.get(name));
+	}
+	
 	//Return ACCOUNT of a given Customer
-	public Account getCustomerAccount(String name) {		
+	public Account getCustomerAccount(String name) {
+		
 		return db.get(name);
 	}
 	
